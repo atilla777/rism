275a5bd3b3d6ad1b7fde14ff185abe0298b56c10
Aleksey Slivka <slivka77@inbox.ru>
2017-10-09 15:44:57 +0300

    Translation was added

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module Rism
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance|
+        "<div class='text-danger has-error' >#{html_tag}</div>".html_safe
+      }
+      config.i18n.default_locale = :ru
+      config.time_zone = 'Moscow'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
