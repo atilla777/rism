14c65f7206c764ad382a507e9fef601156459b4a
Aleksey Slivka <slivka77@inbox.ru>
2017-10-09 11:43:26 +0300

    Organization was cerated

---
+++ db/migrate/20171009072317_create_organizations.rb
@@ -0,0 +1,11 @@
+class CreateOrganizations < ActiveRecord::Migration[5.1]
+  def change
+    create_table :organizations do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+    add_index :organizations, :name
+  end
+end
---
+++ test/system/organizations_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class OrganizationsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit organizations_url
+  #
+  #   assert_selector "h1", text: "Organization"
+  # end
+end
