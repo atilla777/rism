8973ab46ce050dc38c59066ce5ac3815b26b6e09
Aleksey Slivka <slivka77@inbox.ru>
2017-10-10 13:10:40 +0300

    Authlogic was added

--- Gemfile
+++ Gemfile
@@ -54,6 +54,7 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
 
+gem 'authlogic', '~> 3.6.1'
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'bootswatch-rails'
 gem 'font-awesome-rails'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,38 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user_session, :current_user
+  before_filter :authenticate?
+
+  private
+#  def protocol_action(note = '')
+#      @protocol = UserProtocol.new
+#      @protocol.description = note
+#      @protocol.user = current_user
+#      @protocol.ip_address = request.env['REMOTE_ADDR']
+#      @protocol.controller = controller_name
+#      @protocol.action = action_name
+#      @protocol.save
+#  end
+
+  def authenticate?
+      unless current_user
+        redirect_to :sign_in
+      end
+  end
+
+  def current_user_session
+    return @current_user_session if defined?(@current_user_session)
+    @current_user_session = UserSession.find
+  end
+
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = current_user_session && current_user_session.user
+  end
+
+  def user_not_authorized
+    flash[:danger] = t('messages.not_allowed')
+    redirect_to(request.referrer || root_path)
+  end
 end
