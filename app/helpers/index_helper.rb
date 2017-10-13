d5f05bc3f912d79e82bb03eb970fab1ce2d37ceb
Aleksey Slivka <slivka77@inbox.ru>
2017-10-13 09:15:53 +0300

    Move index links to helpers partial

--- app/helpers/index_helper.rb
+++ app/helpers/index_helper.rb
@@ -1,12 +1,39 @@
 module IndexHelper
-  def index_for(records, attributes, &block)
-    attributes.map! do | a |
-      a[:label] ||= records.klass.human_attribute_name(a[:name].to_sym)
-      a
+  class IndexRow
+    attr_reader :record, :fields, :headers
+
+    def initialize(record)
+      @record = record
+      @fields = []
     end
-    render 'helpers/index_records',
-      records: records,
-      attributes: attributes,
-      &block
+
+    def field(value = nil, *headers, &block)
+      if value.present?
+        @fields << value
+      elsif block.present?
+        @fields << yield
+      else
+        @fields << ''
+      end
+    end
+  end
+
+  def index_for(records, *headers, &block)
+    headers.map! do | h |
+      h[:label] ||= records.klass
+                           .human_attribute_name(h[:attribute].to_sym)
+      h
+    end
+    rows = []
+
+    records.each_with_index do | record, i |
+      rows[i] ||= IndexRow.new(record)
+      rows[i] ||= IndexRow.new(record)
+      yield(rows[i], record)
+    end
+
+    render 'helpers/index',
+      headers: headers,
+      rows: rows
   end
 end
