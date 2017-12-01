bfca4623e47f647db1db73ca8c99f83040adb3d0
Aleksey Slivka <slivka77@inbox.ru>
2017-12-01 16:44:03 +0300

    Version decoration refactoring was started

--- app/decorators/version_decorator.rb
+++ app/decorators/version_decorator.rb
@@ -1,20 +1,25 @@
 class  VersionDecorator < SimpleDelegator
   def last_version_info
     result = []
-    if versions.present?
-      result << versions.size
-      result << versions.last.event
-      result << created_at.strftime("%d.%m.%Y-%H:%m")
-      result << User.find(paper_trail.originator).name
+    result << I18n.t('navigations.current_version')
+    if created_at == updated_at
+      result <<  I18n.t("navigations.version_create")
+    else
+      result <<  I18n.t("navigations.version_update")
     end
+    result << updated_at.strftime("%d.%m.%Y-%H:%m")
     result.join(' ')
   end
 
   def version_info
     result = []
-    result << index + 1
+    result << index
     result << "id:#{id}"
-    result << event
+    if event == 'update'
+      result <<  I18n.t("navigations.version_update")
+    else
+      result <<  I18n.t("navigations.version_create")
+    end
     result << created_at.strftime("%d.%m.%Y-%H:%m")
     result << User.find(version_author).name if version_author.present?
     result.join(' ')
