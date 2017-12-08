1ac52e106e63fa3706bf17fe07ab57984734b834
Aleksey Slivka <slivka77@inbox.ru>
2017-12-08 19:09:42 +0300

    Link save problem was fixed

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -7,6 +7,10 @@ class AttachmentsController < ApplicationController
     @attachment = Attachment.new(record_params)
     record = get_record
     if @attachment.save
+      a_l = AttachmentLink.new(attachment_id: @attachment.id,
+                            record_type: params[:attachment][:attachment_link][:record_type],
+                            record_id: params[:attachment][:attachment_link][:record_id])
+      a_l.save
       redirect_to polymorphic_path(record),
                                    success: t('flashes.create', model: Attachment.model_name.human)
     else
@@ -23,5 +27,12 @@ class AttachmentsController < ApplicationController
   def get_record
     model = params[:attachment][:attachment_link][:record_type].camelize.constantize
     model.find(params[:attachment][:attachment_link][:record_id])
+  end
+
+  def record_params
+    params.require(get_model.name.underscore.to_sym)
+          .permit(:name, :document, :organization_id,
+                  attachment_link_attributes: [:id, :record_type, :record_id, :attachment_id])
+          .merge current_user: current_user
   end
 end
