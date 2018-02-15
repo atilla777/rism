4b7d0684cdfab9d011f53452cc13ff205877868f
Aleksey Slivka <slivka77@inbox.ru>
2018-02-15 07:56:51 +0300

    Cpation helper was refactored.

--- app/helpers/caption_helper.rb
+++ app/helpers/caption_helper.rb
@@ -2,23 +2,26 @@
 
 # = Page caption helper
 module CaptionHelper
+  CAPTION_TAG = 'h3'
+  CAPTION_CLASS = 'text-info'
   # Provides method for converting object to text,
   # that can be used as show or index page caption
-  # (title).
+  # (title over a table with records or record attributes).
   #
   # It assumes that bootstrap and slim used in Rails project.
   #
   # Method can take three first argument types:
-  # * one record (model instance)
-  # * several records (ActiveRelation instance)
-  # * text string
+  # * caption(record)   - one record (model instance)
+  # * caption(records)  - several records (ActiveRelation instance)
+  # * caption('text')   - text string
   #
   # You can customize output (set HTML class, id e.t.c) by
-  # set options hash as second argument .
+  # set options hash as second argument.
   #
   # ==== Options
-  # * <tt>:tag</tt> - Specifies html tag (default is h3)
-  # * <tt>:id</tt> - Specifies html id
+  # caption(record, option)
+  # * <tt>:tag</tt>   - Specifies html tag (default is <h3>)
+  # * <tt>:id</tt>    - Specifies html id
   # * <tt>:class</tt> - Specifies html class (default is text-info)
   #
   # ==== Examples
@@ -56,8 +59,8 @@ module CaptionHelper
   private
 
   def set_options(options)
-    options[:tag] ||= 'h3'
-    options[:class] ||= 'text-info'
+    options[:tag] ||= CAPTION_TAG
+    options[:class] ||= CAPTION_CLASS
     options.slice(:tag, :class, :id)
   end
 end
