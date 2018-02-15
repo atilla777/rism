2200b6a91f8434b3d94ede3bd3aaf9caf4b23112
Aleksey Slivka <slivka77@inbox.ru>
2018-02-15 12:31:33 +0300

    Index helper was refactored and documented.

--- app/helpers/caption_helper.rb
+++ app/helpers/caption_helper.rb
@@ -1,28 +1,30 @@
 # frozen_string_literal: true
 
 # = Page caption helper
+# Convert object to text,
+# that can be used as show or index page caption
+# (title over a table with records or record attributes).
+#
+# It assumes that bootstrap and slim is used in the Rails project.
 module CaptionHelper
   CAPTION_TAG = 'h3'
   CAPTION_CLASS = 'text-info'
-  # Provides method for converting object to text,
-  # that can be used as show or index page caption
-  # (title over a table with records or record attributes).
+
+  # Generate page caption
   #
-  # It assumes that bootstrap and slim used in Rails project.
-  #
+  # ==== Attributes
   # Method can take three first argument types:
-  # * caption(record)   - one record (model instance)
-  # * caption(records)  - several records (ActiveRelation instance)
-  # * caption('text')   - text string
-  #
-  # You can customize output (set HTML class, id e.t.c) by
-  # set options hash as second argument.
+  # * +record+   - one record (model instance)
+  # * +records+  - several records (ActiveRelation instance)
+  # * +text+   - text string
   #
   # ==== Options
-  # caption(record, option)
-  # * <tt>:tag</tt>   - Specifies html tag (default is <h3>)
-  # * <tt>:id</tt>    - Specifies html id
-  # * <tt>:class</tt> - Specifies html class (default is text-info)
+  # You can customize output (set HTML tag, class, id) by
+  # set options hash as second argument.
+  #
+  # * +:tag+   - Specifies html tag (default is <h3>)
+  # * +:id+    - Specifies html id
+  # * +:class+  - Specifies html class (default is text-info)
   #
   # ==== Examples
   #  caption(@user)        # => <h3 class='text-primary'>User</h3>
@@ -34,6 +36,9 @@ module CaptionHelper
   #  caption(@record, tag: :span, class: 'text-primary', id: 'caption1')
   #  # => <span class='text-primary' id='caption1'> Record </span>
   def caption(record_or_records_or_text, options = {})
+    options.slice(:tag, :class, :id)
+    options[:tag] ||= CAPTION_TAG
+    options[:class] ||= CAPTION_CLASS
     caption = case record_or_records_or_text
               when ActiveRecord::Relation
                 record_or_records_or_text.klass
@@ -51,16 +56,7 @@ module CaptionHelper
                   'It should be a one record, several records or text string.'
                 )
               end
-    options = set_options(options)
     render('helpers/caption', caption: caption,
                               tag: options)
-  end
-
-  private
-
-  def set_options(options)
-    options[:tag] ||= CAPTION_TAG
-    options[:class] ||= CAPTION_CLASS
-    options.slice(:tag, :class, :id)
   end
 end
--- app/helpers/index_helper.rb
+++ app/helpers/index_helper.rb
@@ -1,7 +1,44 @@
-# frozen_string_literal
+# frozen_string_literal: true
 
-# = Records table for index page helper
+# = Table with records for index page
+# Create index page table
+# with records, sort, filter and RUD (CRUD whithout Create) actions.
+#
+# It assumes that bootstrap, slim and ransack in the Rails project are used.
 module IndexHelper
+  # Generate table with records
+  # ==== Attributes
+  # * +records+ - ActiveRelation instance (displayed records)
+  # * +options+ - custom table visible elements (view Options in section below)
+  # * +block+   - code to work with instance of Index class
+  # ==== Options
+  # +:actions+ - show or not show RUD action for each record
+  # (default is true)
+  # ==== Examples
+  #  = index_for @users do |table|
+  #    - table.header attribute: :name, sort: :desc
+  #    - table.header attribute: :phone, sort: :desc
+  #    - table.header attribute: :organization_id, sort_by: :organization_name, sort: :desc
+  #    - table.body do |r, record|
+  #      - r.field link_to(record.name, record)
+  #      - r.field record.phone
+  #      - r.field link_to(record.organization.name, record.organization)
+  def index_for(records, options = {})
+    options.slice(:actions)
+    options[:actions] = options.fetch(:actions, true)
+    index = Index.new(self, records)
+    yield(index)
+
+    render 'helpers/index',
+           headers: index.headers,
+           rows: index.rows,
+           top_rows: index.top_rows,
+           options: options
+  end
+
+  # == Represent table with records
+  # Instance of that class is transfered as param
+  # to #index_for method block
   class Index
     attr_reader :h, :headers, :rows, :top_rows
 
@@ -13,32 +50,78 @@ module IndexHelper
       @top_rows = []
     end
 
+    # Set table header
+    # ==== Options
+    # * +:attribute+ - record attribute (symbol) wich will
+    # be displayed in table column
+    # (can be ommited, for example in column that not assotoated with
+    # record attribute)
+    # * +:label+     - displayed column caption
+    # (default is a human attribute name, if :attribute is set)
+    # * +:sort+      - sort column direction (:asc or :desc),
+    # column will be sortable only if :sort direction is set
+    # * +:sort_by+   - sort column field (default is like an :attribute),
+    # can be any field in attribute of ActiveRelation record,
+    # fields of associated model id also supported
+    # (this function is delivered by ransack)
+    # ==== Examples
+    #  = index_for @users do |table|
+    #    - table.header / empty column header
+    #    - table.header attribute: :name / not sortable column with user names
+    #    - table.header attribute: :phone, sort: :desc / sortable column with users phones
+    #    - table.header attribute: :organization_id, sort_by: :organization_name, sort: :desc
+    #    / sortable column with associated table (organizations) column
     def header(options)
-      @headers << options
-      @headers.map! do |h|
-        if h[:attribute]
-          h[:label] ||= @records.klass
-                                .human_attribute_name(h[:attribute].to_sym)
-          h[:sort_by] ||= h[:attribute]
-        else
-          h[:label] ||= ''
-        end
-        h
+      if options[:attribute]
+        options[:label] ||= @records
+                            .klass
+                            .human_attribute_name(options[:attribute].to_sym)
+        options[:sort_by] ||= options[:attribute]
+      else
+        options[:label] ||= ''
       end
+      @headers << options.slice(:attribute, :label, :sort_by, :sort)
     end
 
-    def body
-      @records.each_with_index do |record, i|
-        @rows[i] ||= IndexRow.new(@context, record)
-        yield(@rows[i], record)
-      end
-    end
-
+    # Add custom top row to the table
+    # ==== Attributes
+    # *block* - slim code for top row
+    # ==== Examples
+    #  = index_for @users do |table|
+    #    - table.header attribute: :name
+    #    - table.top_row do
+    #      tr
+    #        td
+    #          some text
+    #    - table.body do |row, record|
+    #      - row.field record
     def top_row(&block)
       @top_rows << @context.capture(&block)
     end
+
+    # Set table body
+    # ==== Attributes
+    # * +block+ - block to work with table row,
+    # In that block two parameters are available -
+    # row (instance of a IndexRow class)
+    # and record (instance of ActiveRelation class)
+    # ==== Examples
+    #  = index_for @users do |table|
+    #    - table.header attribute: :name
+    #    - table.body do | row, record |
+    #      - row.field record
+    def body
+      @records.each do |record|
+        row = IndexRow.new(@context, record)
+        @rows << row
+        yield(row, record)
+      end
+    end
   end
 
+  # Represent row in the index page table
+  # Instance of that class is transfered as param
+  # to block in #body method of Index class instance
   class IndexRow
     attr_reader :context, :record, :fields, :headers
 
@@ -50,31 +133,34 @@ module IndexHelper
       @fields = []
     end
 
+    # Set row column (grid table) content (value)
+    # ==== Attributes
+    # * +content+ - text to display in the grid (default is blank - ''),
+    # such text can be created by helper (link_to, for example)
+    # * +options+ - customize grid view (see in Options section)
+    # * +block+   - blcok with slim code wich represent grid content
+    # ====Options
+    # * +:fit+ - if set to true grid will collapse to smallest size
+    # (default is false)
+    # ====Examples
+    #  = index_for @users do |table|
+    #    - table.header attribute: :name
+    #    - table.header attribute: :phone
+    #    - table.header attribute: :organization_id
+    #    - table.header attribute: :job
+    #    - table.header
+    #    - table.body do | row, record |
+    #      - row.field "some text #{record.name}"
+    #      - row.field record.phone, fit: true
+    #      - row.field link_to record.organization, record.organization.name
+    #      - row.field do
+    #        span.text-success
+    #          = record.job
+    #      - row.field
     def field(content = nil, options = {}, &block)
+      options = options.slice(:fit)
       content ||= block.present? ? @context.capture(&block) : ''
       @fields <<  Field.new(content, options)
     end
-  end
-
-  # ===Examples
-  # = index_for @users do
-  #   - i.header attribute: :name, sort: :desc
-  #   - i.header attribute: :organization_id, sort_by: :organization_name, sort: :desc
-  #   - i.header attribute: :phone, sort: :desc
-  #   - i.body do | r, record |
-  #     - r.field link_to(record.name, record)
-  #     - r.field link_to(record.organization.name, record.organization)
-  #     - r.field record.phone
-  # end
-  def index_for(records, options = {})
-    options[:actions] = options.fetch(:actions, true)
-    index = Index.new(self, records)
-    yield(index)
-
-    render 'helpers/index',
-           headers: index.headers,
-           rows: index.rows,
-           top_rows: index.top_rows,
-           options: options
   end
 end
