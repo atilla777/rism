219efe06f56540fa7bbe7e511d038a3eefc8b91e
Aleksey Slivka <slivka77@inbox.ru>
2018-06-10 07:56:24 +0300

    Schedule model was refactored to crontab format

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,7 +1,40 @@
 class Schedule < ApplicationRecord
-  validates :hour, inclusion: { in: 1..24, allow_blank: true }
-  validates :week_day, inclusion: { in: 0..6, allow_blank: true }
-  validates :month_day, inclusion: { in: 1..31, allow_blank: true }
+  validate :check_minutes
+  validate :check_hours
+  validate :check_week_days
+  validate :check_months
+  validate :check_month_days
 
   belongs_to :job, polymorphic: true
+
+  private
+
+  def check_minutes
+    return if check_array(minutes, 0..59)
+    errors.add(:minutes, :invalid)
+  end
+
+  def check_hours
+    return if check_array(hours, 0..23)
+    errors.add(:hours, :invalid)
+  end
+
+  def check_week_days
+    return if check_array(week_days, 0..6)
+    errors.add(:week_days, :invalid)
+  end
+
+  def check_months
+    return if check_array(months, 1..12)
+    errors.add(:months, :invalid)
+  end
+
+  def check_month_days
+    return if check_array(month_days, 1..31)
+    errors.add(:month_days, :invalid)
+  end
+
+  def check_array(array, range)
+    array.is_a?(Array) && array.all?{|value| (range).include?(value)}
+  end
 end
--- db/migrate/20180607174928_create_schedules.rb
+++ db/migrate/20180607174928_create_schedules.rb
@@ -2,9 +2,11 @@ class CreateSchedules < ActiveRecord::Migration[5.1]
   def change
     create_table :schedules do |t|
       t.references :job, polymorphic: true
-      t.integer :hour
-      t.integer :week_day
-      t.integer :month_day
+      t.integer :minutes, array: true, default: []
+      t.integer :hours, array: true, default: []
+      t.integer :month_days, array: true, default: []
+      t.integer :months, array: true, default: []
+      t.integer :week_days, array: true, default: []
 
       t.timestamps
     end
