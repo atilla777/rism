2723081c2c31ed9801379356dca70d8acbd6b46f
Alexey Slivka <slivka77@inbox.ru>
2018-12-03 18:18:00 +0300

    Add lost job status.

--- app/decorators/scan_job_log_decorator.rb
+++ app/decorators/scan_job_log_decorator.rb
@@ -6,12 +6,25 @@ class ScanJobLogDecorator < SimpleDelegator
     end
   end
 
-
   def show_state
     if finish.present?
       "#{I18n.t('labels.scan_job_logs.finished')}"
     else
+      current_state
+    end
+  end
+
+  def working?
+    Sidekiq::Workers.new.find {|_,_, work| work['payload']['jid'] == jid}
+  end
+
+  private
+
+  def current_state
+    if working?
       "#{I18n.t('labels.scan_job_logs.started')}"
+    else
+      "#{I18n.t('labels.scan_job_logs.lost')}"
     end
   end
 end
