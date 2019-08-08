b235949cd873f9380583c7288507e07b1dd62927
Alexey <slivka77@inbox.ru>
2019-08-08 15:36:39 +0300

    Add password complexity

--- app/models/user/devise_config.rb
+++ app/models/user/devise_config.rb
@@ -18,7 +18,7 @@ module User::DeviseConfig
                   end
       c.crypto_provider = Authlogic::CryptoProviders::Sha512
       c.merge_validates_uniqueness_of_email_field_options if: :active
-      c.merge_validates_length_of_password_field_options minimum: 6, if: condition_for_password
+      c.merge_validates_length_of_password_field_options minimum: 8, if: condition_for_password
       c.merge_validates_confirmation_of_password_field_options if: condition_for_password
       c.merge_validates_length_of_password_confirmation_field_options if: condition_for_password
       c.merge_validates_format_of_email_field_options(
@@ -28,9 +28,17 @@ module User::DeviseConfig
     end
 
     before_save :set_activity
+
+    validate :password, :password_complexity
   end
 
   private
+
+  def password_complexity
+    return if password.blank? ||
+      self.password =~ /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/
+    errors.add :password, I18n.t('messages.user.password_complexity_error')
+  end
 
   def set_activity
     return unless active
