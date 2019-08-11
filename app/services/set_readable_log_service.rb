fd469b7e3fb4fc84260d1489da10197832ce5eb5
Aleksey Slivka <slivka77@inbox.ru>
2019-08-11 16:39:11 +0300

    Work in progress

--- app/services/set_readable_log_service.rb
+++ app/services/set_readable_log_service.rb
@@ -11,10 +11,18 @@ class SetReadableLogService
   def execute
     readable_log = ReadableLog.where(
       user_id: @user.id,
-      readable: @readable
+      readable_type: oroginal_class(@readable).name,
+      readable_id: @readable.id
     ).first_or_initialize
     readable_log.read_created_at = @readable.created_at
     readable_log.read_updated_at = @readable.updated_at
     readable_log.save
+  end
+
+  private
+
+  def oroginal_class(obj)
+    return obj.__getobj__.class if obj.respond_to?(:__getobj__)
+    obj.class
   end
 end
