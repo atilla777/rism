dfe9748c5278e19f47e93010131c22cbddf5f25c
Aleksey Slivka <slivka77@inbox.ru>
2020-01-19 09:15:25 +0300

    Work in progeress

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,9 +16,80 @@
 // const imagePath = (name) => images(name, true)
 
 //console.log('Hello World from Webpacker')
-require( '@ckeditor/ckeditor5-build-classic' )
+//require( '@ckeditor/ckeditor5-build-classic' )
+//require( '@ckeditor/ckeditor5-upload' )
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import EssentialsPlugin from '@ckeditor/ckeditor5-essentials/src/essentials';
+import UploadAdapterPlugin from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
+import AutoformatPlugin from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+import BoldPlugin from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import ItalicPlugin from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import BlockQuotePlugin from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+import EasyImagePlugin from '@ckeditor/ckeditor5-easy-image/src/easyimage';
+import HeadingPlugin from '@ckeditor/ckeditor5-heading/src/heading';
+import ImagePlugin from '@ckeditor/ckeditor5-image/src/image';
+import ImageCaptionPlugin from '@ckeditor/ckeditor5-image/src/imagecaption';
+import ImageStylePlugin from '@ckeditor/ckeditor5-image/src/imagestyle';
+import ImageToolbarPlugin from '@ckeditor/ckeditor5-image/src/imagetoolbar';
+import ImageUploadPlugin from '@ckeditor/ckeditor5-image/src/imageupload';
+import LinkPlugin from '@ckeditor/ckeditor5-link/src/link';
+import ListPlugin from '@ckeditor/ckeditor5-list/src/list';
+import ParagraphPlugin from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+
 document.addEventListener('turbolinks:load', () => {
-  ClassicEditor.create(document.querySelector('.ckeditor'), {
-    simpleUpload: { uploadUrl: '/uploads' },
-  })
+  ClassicEditor.create(document.querySelector('.ckeditor2'), {
+        plugins: [
+            EssentialsPlugin,
+            AutoformatPlugin,
+            BoldPlugin,
+            ItalicPlugin,
+            BlockQuotePlugin,
+            HeadingPlugin,
+            ImagePlugin,
+            ImageCaptionPlugin,
+            ImageStylePlugin,
+            ImageToolbarPlugin,
+            EasyImagePlugin,
+            ImageUploadPlugin,
+            LinkPlugin,
+            ListPlugin,
+            ParagraphPlugin,
+            UploadAdapterPlugin
+        ],
+
+        simpleUpload: { uploadUrl: '/uploads' },
+
+        //toolbar: ["undo", "redo", "bold", "italic", "blockQuote", "ckfinder", "imageTextAlternative", "imageUpload", "heading", "imageStyle:full", "imageStyle:side", "indent", "outdent", "link", "numberedList", "bulletedList", "mediaEmbed", "insertTable", "tableColumn", "tableRow", "mergeTableCells"],
+        toolbar: [
+            'heading',
+            'bold',
+            'italic',
+            'link',
+            'bulletedList',
+            'numberedList',
+            'imageUpload',
+            'blockQuote',
+            'undo',
+            'redo'
+        ],
+        //toolbar: ["bold", "italic", "heading"],
+
+        heading: {
+            options: [
+                { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+            ],
+         image: {
+            toolbar: [
+                'imageStyle:full',
+                'imageStyle:side',
+                '|',
+                'imageTextAlternative'
+            ]
+        }
+    }
+  })//.then( editor => {
+    //    console.log( Array.from( editor.ui.componentFactory.names() ) );
+    //} )
 })
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,46 @@
 const { environment } = require('@rails/webpacker')
 
+
+const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
+const { styles } = require( '@ckeditor/ckeditor5-dev-utils' );
+
+module.exports = {
+    plugins: [
+        // ...
+
+        new CKEditorWebpackPlugin( {
+            // See https://ckeditor.com/docs/ckeditor5/latest/features/ui-language.html
+            language: 'ru'
+        } )
+    ],
+module: {
+        rules: [
+            {
+                test: /ckeditor5-[^/\\]+[/\\]theme[/\\]icons[/\\][^/\\]+\.svg$/,
+                use: [ 'raw-loader' ]
+            },
+            {
+                test: /ckeditor5-[^/\\]+[/\\]theme[/\\].+\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                        options: {
+                            injectType: 'singletonStyleTag'
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: styles.getPostCssConfig( {
+                            themeImporter: {
+                                themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
+                            },
+                            minify: true
+                        } )
+                    },
+                ]
+            }
+        ]
+    }
+};
+
 module.exports = environment
