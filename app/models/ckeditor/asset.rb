2bc3a54ca31aaae7631967abcf23d28f4dd657d5
Alexey <slivka77@inbox.ru>
2020-01-23 14:33:07 +0300

    Fix error

--- app/models/ckeditor/asset.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Ckeditor::Asset < ActiveRecord::Base
-  include Ckeditor::Orm::ActiveRecord::AssetBase
-
-  delegate :url, :current_path, :content_type, to: :data
-
-  validates :data, presence: true
-end
--- app/models/ckeditor/attachment_file.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Ckeditor::AttachmentFile < Ckeditor::Asset
-  mount_uploader :data, CkeditorAttachmentFileUploader, mount_on: :data_file_name
-
-  def url_thumb
-    @url_thumb ||= Ckeditor::Utils.filethumb(filename)
-  end
-end
