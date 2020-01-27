87f6dbd934e75d11b090293a7fc46c9c1de27058
Alexey <slivka77@inbox.ru>
2020-01-27 10:32:43 +0300

    Clear old ckeditor assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,5 +12,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-# Rails.application.config.public_file_server.enabled = true
+ Rails.application.config.public_file_server.enabled = true
 # Rails.application.config.assets.precompile += %w(article.css)
