859e236698729bd5f1c394dd1ffd405178519403
Alexey Slivka <slivka77@inbox.ru>
2020-03-30 11:56:31 +0300

    work in progress

--- app/controllers/api/custom_reports_controller.rb
+++ app/controllers/api/custom_reports_controller.rb
@@ -3,6 +3,7 @@
 class Api::CustomReportsController < ApplicationController
   include ActionController::HttpAuthentication::Token::ControllerMethods
 
+  # TODO: uncomment to use authorization
   #before_action :authenticate
 
   skip_before_action :set_paper_trail_whodunnit
@@ -10,11 +11,12 @@ class Api::CustomReportsController < ApplicationController
   skip_after_action :log_user_action
   skip_after_action :verify_authorized
 
+  # TODO: Add option to command: -H 'Authorization: Token token="afbadb4ff8485c0adcba486b4ca90cc4"'
   # Download custom report file throught API.
   # Examles of usage (3 is a custom report ID):
   # download as file -
   # curl -O -J http://localhost:3000/api/custom_reports/3
-  # show downloaded content in console (or transfer it to another app through pipe) -
+  # show downloaded content in console -
   # curl http://localhost:3000/api/custom_reports/3
   # transfer downloaded content to another app through pipe -
   # curl http://localhost:3000/api/custom_reports/3 | grep some_text
@@ -36,7 +38,7 @@ class Api::CustomReportsController < ApplicationController
 
   def authenticate_token
     authenticate_with_http_token do |token, options|
-      @current_user = User.find_by(api_key: token)
+      @current_user = User.find_by(api_key: token) # TODO: Add api_token field to user - SecureRandom.hex
     end
   end
 
