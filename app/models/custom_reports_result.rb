5e670bf31cd387bcd1cdc366945fbf6db4408446
Alexey Slivka <slivka77@inbox.ru>
2020-04-27 17:05:51 +0300

    Remove old custom reports results when new one created

--- app/models/custom_reports_result.rb
+++ app/models/custom_reports_result.rb
@@ -2,6 +2,7 @@ class CustomReportsResult < ApplicationRecord
   include OrganizationAssociated
   include Rightable
 
+  after_create :remove_old_results
   before_destroy :delete_result_file
 
   validates :custom_report_id, numericality: { only_integer: true, allow_blank: true }
@@ -23,7 +24,15 @@ class CustomReportsResult < ApplicationRecord
 
   private
 
-  # Delete folder with article images
+  # Remove old results (stay only 5 last)
+  def remove_old_results
+    CustomReportsResult.where(custom_report_id: custom_report_id)
+      .order('created_at DESC')
+      .offset(5)
+      .destroy_all
+  end
+
+  # Delete folder with report results files
   def delete_result_file
     return unless result_path.present?
     file_path = result_file_path
