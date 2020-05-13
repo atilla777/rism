35c578e5b6dce58546a33e5284139b8c45e4f3fa
Alexey Slivka <slivka77@inbox.ru>
2020-05-13 14:16:52 +0300

    Sort port in shodan

--- app/decorators/enrichment_decorator.rb
+++ app/decorators/enrichment_decorator.rb
@@ -10,4 +10,14 @@ class EnrichmentDecorator < SimpleDelegator
   def show_broker
     Enrichment.human_enum_name(:broker, broker)
   end
+
+  def show_link_to_service
+    case broker
+    when 'shodan'
+      "https://www.shodan.io/host/#{enrichmentable.content}"
+    when 'virus_total'
+      "https://www.virustotal.com/gui/ip-address/#{enrichmentable.content}/detection"
+    end
+  end
+
 end
